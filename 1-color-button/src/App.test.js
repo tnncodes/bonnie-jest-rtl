acdb60ccf58948f1201339a43658f1348fdaa5ce
tnncodes <tnncodes@gmail.com>
2022-08-27 16:26:14 -0300

    color-button: button turns blue when clicked

--- 1-color-button/src/App.test.js
+++ 1-color-button/src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('button has correct initial color', () => {
@@ -9,4 +9,19 @@ test('button has correct initial color', () => {
 
   // expect the background color to be red
   expect(colorButton).toHaveStyle({ backgroundColor: 'red' })
+});
+
+test('button turns blue when clicked', () => {
+  render(<App />);
+
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+
+  // click button
+  fireEvent.click(colorButton);
+
+  // expect the background color to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+
+  // expect the button text to be 'Change to red'
+  expect(colorButton.textContent).toBe('Change to red');
 });
