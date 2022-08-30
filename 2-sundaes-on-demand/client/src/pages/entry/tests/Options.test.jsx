ecd7d1c3e4fadc76f8a0a2d8128ee2a1f0f1da3c
tnncodes <tnncodes@gmail.com>
2022-08-30 17:53:40 -0300

    sundaes-on-demand: update test scoop options

--- 2-sundaes-on-demand/client/src/pages/entry/tests/Options.test.jsx
+++ 2-sundaes-on-demand/client/src/pages/entry/tests/Options.test.jsx
@@ -1,5 +1,5 @@
-// import { render, screen } from '../../../test-utils/testing-library-utils';
-import { render, screen } from '@testing-library/react';
+import { render, screen } from '../../../test-utils/testing-library-utils';
+// import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import Options from '../Options';
