0b2276af6ecbc98f1f1d041903cb8538fa3b3a32
tnncodes <tnncodes@gmail.com>
2022-08-31 16:34:57 -0300

    sundaes-on-demand: toppings subtotal

--- 2-sundaes-on-demand/client/src/pages/entry/tests/totalUpdates.test.jsx
+++ 2-sundaes-on-demand/client/src/pages/entry/tests/totalUpdates.test.jsx
@@ -25,3 +25,28 @@ test('update scoop subtotal when scoops change', async () => {
   userEvent.type(chocolateInput, '2');
   expect(scoopsSubtotal).toHaveTextContent('6.00');
 });
+
+test('update toppings subtotal when toppings change', async () => {
+  // render parent component
+  render(<Options optionType="toppings" />);
+
+  // make sure total starts out at $0.00
+  const toppingsTotal = screen.getByText('Toppings total: $', { exact: false });
+  expect(toppingsTotal).toHaveTextContent('0.00');
+
+  // add cherries and check subtotal
+  const cherriesCheckbox = await screen.findByRole('checkbox', {
+    name: 'Cherries',
+  });
+  userEvent.click(cherriesCheckbox);
+  expect(toppingsTotal).toHaveTextContent('1.50');
+
+  // add hot fudge and check subtotal
+  const hotFudgeCheckbox = screen.getByRole('checkbox', { name: 'Hot fudge' });
+  userEvent.click(hotFudgeCheckbox);
+  expect(toppingsTotal).toHaveTextContent('3.00');
+
+  // remove hot fudge and check subtotal
+  userEvent.click(hotFudgeCheckbox);
+  expect(toppingsTotal).toHaveTextContent('1.50');
+});
